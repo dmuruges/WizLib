a3a9d944e19c5f9b27303ccd5f7b1ede97a54e54
Dhanasekar Murugesan <sekar.murugesan@gmail.com>
2020-11-30 01:35:09 +0530

    Deleted DisplayOrder Column from Genres Table

--- WizLib_Models/Models/Genre.cs
+++ WizLib_Models/Models/Genre.cs
@@ -8,6 +8,6 @@ namespace WizLib_Model.Models
     {
         public int GenreId { get; set; }
         public string GenreName { get; set; }
-        public int DisplayOrder { get; set; }
+        //public int DisplayOrder { get; set; }
     }
 }
