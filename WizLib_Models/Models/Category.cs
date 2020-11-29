0263b1379a464521bd3285a591f847f1e7e95c5d
Dhanasekar Murugesan <sekar.murugesan@gmail.com>
2020-11-29 21:51:48 +0530

    Implemented Category Model to WizLib_Model Project

---
+++ WizLib_Models/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WizLib_Model.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
